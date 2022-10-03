b8ad8b15084f5b46ca8958485d28078d329ed458
Diego Orlando Gonzalez Cruz <dogc.1994@gmail.com>
2022-10-03 08:16:42 -0500

    firts COmmit

---
+++ ArquitecturaCruds/src/main/java/com/plat/mvs/ArquitecturaCrudsApplication.java
@@ -0,0 +1,13 @@
+package com.plat.mvs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquitecturaCrudsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArquitecturaCrudsApplication.class, args);
+    }
+
+}
---
+++ ArquitecturaCruds/src/test/java/com/plat/mvs/ArquitecturaCrudsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plat.mvs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquitecturaCrudsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
